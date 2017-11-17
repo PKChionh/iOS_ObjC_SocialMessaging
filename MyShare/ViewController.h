d067eebfbde4a5427b0f8585b29051c13be180e8
ITSAdmin <ITSAdmin@cw4305mac41.local>
2017-11-17 10:17:39 +0800

    Initial Commit

---
+++ MyShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyShare
+//
+//  Created by ITSAdmin on 17/11/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
